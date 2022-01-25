eca8287b2b569b26ad2b9ea4f68838528e8866cb
Staff Asia <staffasia@Staffs-iMac.local>
2022-01-25 16:33:14 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const sliders = document.querySelector('.sliders');
+
+const slider = Array.from(document.getElementsByClassName('slider'));
+
+// Buttons
+
+const previousBtn = document.getElementById('button-left');
+const nextBtn = document.getElementById('button-right');
+
+
+let counter = 0;
+
+
+nextBtn.addEventListener('click', () => {
+    counter = (counter < 5) ? counter + 1 : 0;
+    sliders.style.transform = 'translate(' + (counter) * -15 + '%)';
+})
+
+
+previousBtn.addEventListener('click', () => {
+    counter = (counter > 0) ? counter - 1 : 5;
+    sliders.style.transform = 'translate(' + (counter) * -15 + '%)';
+})
+
+
