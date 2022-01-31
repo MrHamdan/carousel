610a4353a3ed89c7e39829129bbed66d13c44227
Staff Asia <staffasia@Staffs-iMac.local>
2022-01-31 17:06:18 +0600

    dots navigation are added

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ const slider = Array.from(document.getElementsByClassName('slider'));
 const previousBtn = document.getElementById('button-left');
 const nextBtn = document.getElementById('button-right');
 
+const indicatorParent = document.querySelector('.radios ul');
+const indicators = document.querySelectorAll('.radios li');
+
+indicators.forEach((indicator, i) => {
+    indicator.addEventListener('click', () => {
+        document.querySelector('.radios .selected').classList.remove('selected');
+        indicator.classList.add('selected');
+        sliders.style.transform = 'translateX(' + (i) * -15 + '%)';
+        counter = i;
+    });
+});
+
 
 let counter = 0;
 
 // Slider Autoplay
 setInterval(() => {
     counter = (counter < 5) ? counter + 1 : 0;
+    document.querySelector('.radios .selected').classList.remove('selected');
+    indicatorParent.children[counter].classList.add('selected');
     sliders.style.transform = 'translate(' + (counter) * -15 + '%)';
 
 }, 8000);
@@ -21,12 +35,16 @@ setInterval(() => {
 // Slider Buttons
 nextBtn.addEventListener('click', () => {
     counter = (counter < 5) ? counter + 1 : 0;
+    document.querySelector('.radios .selected').classList.remove('selected');
+    indicatorParent.children[counter].classList.add('selected');
     sliders.style.transform = 'translate(' + (counter) * -15 + '%)';
 })
 
 
 previousBtn.addEventListener('click', () => {
     counter = (counter > 0) ? counter - 1 : 5;
+    document.querySelector('.radios .selected').classList.remove('selected');
+    indicatorParent.children[counter].classList.add('selected');
     sliders.style.transform = 'translate(' + (counter) * -15 + '%)';
 })
 
